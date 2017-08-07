c4f5a2761f411b76e4d241fd72dad99f6a25001f
Acharya <priyankitastro10@gmail.com>
2017-08-07 21:02:53 +0530

    minor changes

--- line_follower-PID.ino
+++ line_follower-PID.ino
@@ -1,27 +1,29 @@
 #include <QTRSensors.h>
 
-#define Kp 8 // experiment to determine this, start by something small that just makes your bot follow the line at a slow speed
-#define Kd  161 // experiment to determine this, slowly increase the speeds and adjust this value. ( Note: Kp < Kd) 
-#define RIGHT_MAX_SPEED 200 // max speed of the robot
-#define LEFT_MAX_SPEED 200 // max speed of the robot
-#define RIGHT_BASE_SPEED 250 // this is the speed at which the motors should spin when the robot is perfectly on the line
-#define LEFT_BASE_SPEED 250  // this is the speed at which the motors should spin when the robot is perfectly on the line
-#define NUM_SENSORS  6     // number of sensors used
-#define TIMEOUT       4  // waits for 2500 us for sensor outputs to go low
-#define EMITTER_PIN   2     // emitter is controlled by digital pin 2
+// Sensors used -  Pololu QTR-8RC array 
+// FINDING Kp AND Kd IS ARDOUS
 
-#define rightMotor1 8
-#define rightMotor2 9
+#define Kp 10                     // change it by less margin that just makes your bot follow the line at a slow speed
+#define Kd  200                   // change it slowly and Kp < Kd 
+#define RIGHT_MAX_SPEED 100 
+#define LEFT_MAX_SPEED 100 
+#define RIGHT_BASE_SPEED 150      // speed of motors  when the robot is perfectly on the line
+#define LEFT_BASE_SPEED 150       // speed of motors  when the robot is perfectly on the line
+#define NUM_SENSORS  6            // number of sensors used
+#define TIMEOUT 4                 // waits for 2500 us for sensor outputs to go low
+#define EMITTER_PIN 2             // emitter is controlled by digital pin 2
+
+#define rightMotor1 8     // FORWARD
+#define rightMotor2 9     // BACKWARD
 #define rightMotorPWM 10
-#define leftMotor1 13 // enable left
-#define leftMotor2 12
+#define leftMotor1 13     // FORWARD
+#define leftMotor2 12     // BACKWARD
 #define leftMotorPWM 11
 #define motorPower 7
 
 QTRSensorsAnalog qtrrc((unsigned char[]) {
-  14, 15, 16, 17, 18, 19
-} , NUM_SENSORS, TIMEOUT, EMITTER_PIN); // sensor connected through analog pins A0 - A5 i.e. digital pins 14-19
-
+  14, 15, 16, 17, 18, 19                            // A0 - A5
+} , NUM_SENSORS, TIMEOUT, EMITTER_PIN); 
 unsigned int sensorValues[NUM_SENSORS];
 
 void setup()
@@ -35,12 +37,9 @@ void setup()
   pinMode(motorPower, OUTPUT);
   Serial.begin(9600);
   int i;
-  for (int i = 0; i < 400; i++) // calibrate for sometime by sliding the sensors across the line, or you may use auto-calibration instead
-
-    qtrrc.calibrate();
-  //  delay(20);
-  // wait for 2s to position the bot before entering the main loop
-
+  for (int i = 0; i < 400; i++) 
+    qtrrc.calibrate();                              // MOST IMPORTANT PART
+    delay(20);
 }
 
 int lastError = 0;
@@ -48,7 +47,7 @@ int lastError = 0;
 void loop()
 {
   unsigned int sensors[6];
-  int position = qtrrc.readLine(sensors); // get calibrated readings along with the line position, refer to the QTR Sensors Arduino Library for more details on line position.
+  int position = qtrrc.readLine(sensors); 
   int error = position - 2500;
   Serial.println(position);
   int motorSpeed = Kp * error + Kd * (error - lastError);
@@ -57,13 +56,19 @@ void loop()
   int rightMotorSpeed = RIGHT_BASE_SPEED + motorSpeed;
   int leftMotorSpeed = LEFT_BASE_SPEED - motorSpeed;
 
-  if (rightMotorSpeed > RIGHT_MAX_SPEED ) rightMotorSpeed = RIGHT_MAX_SPEED; // prevent the motor from going beyond max speed
-  if (leftMotorSpeed > LEFT_MAX_SPEED ) leftMotorSpeed = LEFT_MAX_SPEED; // prevent the motor from going beyond max speed
-  if (rightMotorSpeed < 0) rightMotorSpeed = 0; // keep the motor speed positive
-  if (leftMotorSpeed < 0) leftMotorSpeed = 0; // keep the motor speed positive
-
-  {
-    digitalWrite(motorPower, HIGH); // move forward with appropriate speeds
+  if (rightMotorSpeed > RIGHT_MAX_SPEED ) 
+    rightMotorSpeed = RIGHT_MAX_SPEED; 
+  
+  if (leftMotorSpeed > LEFT_MAX_SPEED ) 
+    leftMotorSpeed = LEFT_MAX_SPEED; 
+  
+  if (rightMotorSpeed < 0) 
+    rightMotorSpeed = 0;
+  
+  if (leftMotorSpeed < 0)
+    leftMotorSpeed = 0; 
+  
+    digitalWrite(motorPower, HIGH); 
     digitalWrite(rightMotor1, HIGH);
     digitalWrite(rightMotor2, LOW);
     analogWrite(rightMotorPWM, rightMotorSpeed);
@@ -71,7 +76,7 @@ void loop()
     digitalWrite(leftMotor1, HIGH);
     digitalWrite(leftMotor2, LOW);
     analogWrite(leftMotorPWM, leftMotorSpeed);
-  }
+  
 }
 
 
